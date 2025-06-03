e237c3aa8b8003ff72e32c62f821a0170d8ef6c7
Amalie Dobel Christiansen <adc@kvalitetsit.dk>
2025-06-03 15:19:32 +0200

    Initial v2 controller

---
+++ service/src/main/java/dk/medcom/vdx/organisation/controller/v2/OrganisationV2Controller.java
@@ -0,0 +1,47 @@
+package dk.medcom.vdx.organisation.controller.v2;
+
+import org.openapitools.api.OrganisationV2Api;
+import org.openapitools.model.Organisation;
+import org.openapitools.model.OrganisationCreate;
+import org.openapitools.model.OrganisationUriInner;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class OrganisationV2Controller implements OrganisationV2Api {
+    private final static Logger logger = LoggerFactory.getLogger(OrganisationV2Controller.class);
+
+    @Override
+    public ResponseEntity<Organisation> servicesV2OrganisationCodeGet(String code) {
+        logger.debug("Enter GET organisation from code in path.");
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<Organisation> servicesV2OrganisationGet(String organisationCode) {
+        logger.debug("Enter GET organisation from code in query.");
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<Organisation> servicesV2OrganisationParentCodePost(String parentCode, OrganisationCreate organisationCreate) {
+        logger.debug("Enter POST organisation where parent code in path.");
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<Organisation> servicesV2OrganisationPost(String parentCode, OrganisationCreate organisationCreate) {
+        logger.debug("Enter POST organisation where parent code in query.");
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<List<OrganisationUriInner>> servicesV2OrganisationUriPost(List<String> requestBody) {
+        logger.debug("Enter POST organisation uri.");
+        return null;
+    }
+}
