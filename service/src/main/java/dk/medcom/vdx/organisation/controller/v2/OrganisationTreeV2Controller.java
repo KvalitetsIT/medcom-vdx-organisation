1d9a2c4b0287496f93bb807d0fb029cf35d05835
Amalie Dobel Christiansen <adc@kvalitetsit.dk>
2025-06-03 15:19:32 +0200

    Initial v2 controller

---
+++ service/src/main/java/dk/medcom/vdx/organisation/controller/v2/OrganisationTreeV2Controller.java
@@ -0,0 +1,38 @@
+package dk.medcom.vdx.organisation.controller.v2;
+
+import org.openapitools.api.OrganisationTreeV2Api;
+import org.openapitools.model.OrganisationTreeForApiKey;
+import org.openapitools.model.Organisationtree;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class OrganisationTreeV2Controller implements OrganisationTreeV2Api {
+    private final static Logger logger = LoggerFactory.getLogger(OrganisationTreeV2Controller.class);
+
+    @Override
+    public ResponseEntity<Organisationtree> servicesV2OrganisationTreeForApiKeyPost(OrganisationTreeForApiKey organisationTreeForApiKey) {
+        logger.debug("Enter POST organisation tree for api key.");
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<Organisationtree> servicesV2OrganisationtreeChildrenGet(String organisationCode, Integer groupId) {
+        logger.debug("Enter GET organisation tree children by query parameter.");
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<Organisationtree> servicesV2OrganisationtreeCodeGet(String code) {
+        logger.debug("Enter GET organisation tree from code.");
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<Organisationtree> servicesV2OrganisationtreeGet(String organisationCode, Integer groupId) {
+        logger.debug("Enter GET organisation tree by query parameter.");
+        return null;
+    }
+}
