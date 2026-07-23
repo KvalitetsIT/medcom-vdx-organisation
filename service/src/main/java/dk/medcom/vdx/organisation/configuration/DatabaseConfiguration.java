f049b79236bcfa2156f6c948e509981740f37843
fma-spec <fma@kvalitetsit.dk>
2026-07-23 14:30:18 +0200

    bump org.springframework.boot from 3.5.15 to 4.1.0

--- service/src/main/java/dk/medcom/vdx/organisation/configuration/DatabaseConfiguration.java
+++ service/src/main/java/dk/medcom/vdx/organisation/configuration/DatabaseConfiguration.java
@@ -6,7 +6,7 @@ import org.flywaydb.core.Flyway;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.persistence.autoconfigure.EntityScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
